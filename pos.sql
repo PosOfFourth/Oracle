e0aebff5f83deded527053f8160bba9429c3662c
minseo91 <99579231+minseo91@users.noreply.github.com>
2022-03-07 00:03:05 +0900

    Add files via upload

--- pos.sql
+++ pos.sql
@@ -5,7 +5,7 @@ GRANT CONNECT, RESOURCE, DBA TO C##TEAM;
 
 CREATE TABLE CUSTOMER( --유저
  CUS_PHONE VARCHAR2(20) PRIMARY KEY, --핸드폰번호
- CUS_POINT NUMBER NOT NULL --포인트 (주문금액의 10프로씩 적립)
+ --CUS_POINT NUMBER NOT NULL --포인트 (주문금액의 10프로씩 적립)
 );
 
 UPDATE CUSTOMER SET CUS_POINT WHERE 총금액*10 -----???????
@@ -16,27 +16,26 @@ SELECT * FROM CUSTOMER
 ----------------------------------------------------------------------------------------------
 
 CREATE TABLE ORDERS( --주문
- ORDER_ID VARCHAR2(20) PRIMARY KEY, --주문번호
+ ORDER_ID VARCHAR2(50) PRIMARY KEY, --주문번호
  CUS_PHONE VARCHAR2(20) REFERENCES CUSTOMER(CUS_PHONE), --핸드폰번호
  ORDER_DATE DATE --날짜
 );
 
-CREATE SEQUENCE ORDERS_SEQ;
-DROP SEQUENCE ORDERS_SEQ;
+DROP TABLE ORDERS
 
-INSERT INTO ORDERS VALUES ('JB ' || ORDERS_SEQ.NEXTVAL, '01029235057' , SYSDATE)
+INSERT INTO ORDERS VALUES ('', '01029235057', SYSDATE)
 
 SELECT * FROM ORDERS
 DELETE FROM ORDERS WHERE CUS_PHONE = '01029235057'
 
 ------------------------------------------------------------------------------------------------------
 
-CREATE TABLE GOODS( --판매상품(아이스크림)
+CREATE TABLE GOODS( --판매상품(아이스크림) 
  GOODS_ID VARCHAR2(20) PRIMARY KEY, --상품코드
  GOODS_NAME VARCHAR2(20) NOT NULL, --상품이름
  STOCK_QTY NUMBER , --재고수량
- GOODS_PRICE NUMBER  --소비자가격
-);
+ GOODS_PRICE NUMBER  --소비자가격         
+);  -----------------콘솔창에 상품코드는 안보이게 하는 방법???
 
 INSERT INTO GOODS VALUES('GC01_SP01' , '구구콘' , 0 ,1500);
 INSERT INTO GOODS VALUES('GC01_SP02' , '빵빠레' , 0 ,1500); 
@@ -65,19 +64,22 @@ INSERT INTO GOODS VALUES('GC04_SP05' , '체리마루', 0 ,7000); --컵
 UPDATE GOODS SET STOCK_QTY=STOCK_QTY +30 --맨 처음물량 들어왔을때 
 SELECT * FROM GOODS
 
---재고량이 5개 이하일시 자동으로 발주하게끔 하려면 어떻게 해야하는지....
-
 ------------------------------------------------------------------------------------------------
 
 CREATE TABLE ORDER_LINE( --주문상세
- ORDER_LINE_ID VARCHAR2(20) PRIMARY KEY, --주문상세코드
- ORDER_ID VARCHAR2(20) REFERENCES ORDERS(ORDER_ID), --주문번호
+ ORDER_LINE_ID VARCHAR2(30) PRIMARY KEY, --주문상세코드
+ ORDER_ID VARCHAR2(50) REFERENCES ORDERS(ORDER_ID), --주문번호
  GOODS_ID VARCHAR2(20) REFERENCES GOODS(GOODS_ID), --상품코드
  ORDER_PRICE NUMBER , --가격
  ORDER_QTY NUMBER --주문수량
 );
 
-INSERT INTO ORDER_LINE VALUES(JS);
+SELECT * FROM ORDER_LINE
+DROP TABLE ORDER_LINE
+INSERT INTO ORDER_LINE VALUES('JS ' || ORDER_LINE_SEQ.NEXTVAL,, 'GC01_SP01', 1500, 1);
+
+CREATE SEQUENCE ORDER_LINE_SEQ;
+DROP SEQUENCE ORDER_LINE_SEQ; 
 
 -----------------------------------------------------------------------------------------------
 
@@ -130,31 +132,112 @@ DROP SEQUENCE IB_LINE_SEQ;
 
 SELECT * FROM IB_LINE;
 
-INSERT INTO IB_LINE VALUES(???,'BJ ' || IB_LINE_SEQ.NEXTVAL ,'GC01_SP01' ,30,750);   ------발주상세코드도 시퀀스????? 
+INSERT INTO IB_LINE VALUES('BS ' || IB_LINE_SEQ.NEXTVAL, ,'GC01_SP01',30,750);   ------발주상세코드도 시퀀스????? 
 DELETE FROM IB_LINE WHERE GOODS_ID = 'GC01_SP01'
 
 ----------------------------------------------------------------------------------------------------------
 
-CREATE TABLE ICE( --취급상품
- ICE_ID VARCHAR2(30) PRIMARY KEY, --거래처취급상품
+CREATE TABLE SKU( --취급상품
+ SKU_ID VARCHAR2(30) PRIMARY KEY, --거래처취급상품
  DEALER_ID VARCHAR2(20) REFERENCES DEALER(DEALER_ID), --거래처코드
- DEALER_PRICE NUMBER , --거래처가격
- ICE_TYPE VARCHAR2(20) NOT NULL, --종류
- ICE_QTY NUMBER --거래처 자체 수량
+ SKU_PRICE NUMBER , --거래처가격
+ SKU_TYPE VARCHAR2(20) NOT NULL --종류
 );
 
+--ICE_QTY NUMBER --거래처 자체 수량
+
 --거래처가격은 발주가격의 30%로 책정함 
-INSERT INTO ICE VALUES('GP01', 'GC01', 525, '콘', 999); 
-INSERT INTO ICE VALUES('GP02', 'GC02', 280, '쭈쭈바', 999);
-INSERT INTO ICE VALUES('GP03', 'GC03', 140, '막대', 999);
-INSERT INTO ICE VALUES('GP04', 'GC04', 2450, '컵', 999);
+INSERT INTO SKU VALUES('GP01', 'GC01', 525, '콘'); 
+INSERT INTO SKU VALUES('GP02', 'GC02', 280, '쭈쭈바');
+INSERT INTO SKU VALUES('GP03', 'GC03', 140, '막대');
+INSERT INTO SKU VALUES('GP04', 'GC04', 2450, '컵');
 
-SELECT * FROM ICE; 
+SELECT * FROM SKU; 
 
-UPDATE ICE SET ICE_QTY=ICE_QTY-30  -- 맨 처음 주문할때 
-UPDATE ICE SET ICE_QTY=ICE_QTY-10 WHERE DEALER_ID = 'GC02'
+--UPDATE ICE SET ICE_QTY=ICE_QTY-30  -- 맨 처음 주문할때 
+--UPDATE ICE SET ICE_QTY=ICE_QTY-10 WHERE DEALER_ID = 'GC02'
+
+DROP TABLE SKU
+
+---------------------------------------------------------------------------------------------
+CREATE TABLE ICE ( --아이스크림전체
+ SKU_TYPE VARCHAR2(20) PRIMARY KEY,-- 아이스크림 종류
+ ICE_ID VARCHAR2(20) NOT NULL --아이스크림 이름
+); 
+
+INSERT INTO ICE VALUES ('콘' , '구구콘 마다가스카르바닐라맛');
+INSERT INTO ICE VALUES ('콘' , '구구콘 오리지널맛');
+INSERT INTO ICE VALUES ('콘' , '구구콘 스트로베리맛');
+INSERT INTO ICE VALUES ('콘' , '구구콘 화이트초코맛');
+INSERT INTO ICE VALUES ('콘' , '구구콘 피넛버터맛');
+
+INSERT INTO ICE VALUES ('콘' , '빵빠레 바닐라맛');
+INSERT INTO ICE VALUES ('콘' , '빵빠레 초코맛');
+INSERT INTO ICE VALUES ('콘' , '빵빠레 딸기맛');
+
+INSERT INTO ICE VALUES ('콘' , '슈퍼콘 쿠앤크맛');
+INSERT INTO ICE VALUES ('콘' , '슈퍼콘 민트초코칩맛');
+INSERT INTO ICE VALUES ('콘' , '슈퍼콘 바닐라맛');
+INSERT INTO ICE VALUES ('콘' , '슈퍼콘 딸기맛');
+INSERT INTO ICE VALUES ('콘' , '슈퍼콘 초코맛');
+
+INSERT INTO ICE VALUES ('콘' , '월드콘 바닐라맛');
+INSERT INTO ICE VALUES ('콘' , '월드콘 초코맛');
+INSERT INTO ICE VALUES ('콘' , '월드콘 쿠키앤크림맛');
+INSERT INTO ICE VALUES ('콘' , '월드콘 애플크럼블맛');
+INSERT INTO ICE VALUES ('콘' , '월드콘 까마로사딸기맛');
+
+INSERT INTO ICE VALUES ('콘' , '부라보콘 피스타치오맛');
+INSERT INTO ICE VALUES ('콘' , '부라보콘 초코청크맛');
+INSERT INTO ICE VALUES ('콘' , '부라보콘 바닐라맛');
+
+INSERT INTO ICE VALUES ('쭈쭈바' , '');
+INSERT INTO ICE VALUES ('쭈쭈바' , '');
+INSERT INTO ICE VALUES ('쭈쭈바' , '');
+INSERT INTO ICE VALUES ('쭈쭈바' , '');
+INSERT INTO ICE VALUES ('쭈쭈바' , '');
+
+INSERT INTO ICE VALUES ('막대' , '스크류바 딸기맛');
+INSERT INTO ICE VALUES ('막대' , '스크류바 복숭아맛');
+INSERT INTO ICE VALUES ('막대' , '스크류바 사과맛');
+INSERT INTO ICE VALUES ('막대' , '스크류바 포도맛');
+INSERT INTO ICE VALUES ('막대' , '스크류바 오렌지맛');
+
+INSERT INTO ICE VALUES ('막대' , '죠스바 오렌지맛');
+INSERT INTO ICE VALUES ('막대' , '죠스바 파인애플맛');
+INSERT INTO ICE VALUES ('막대' , '죠스바 수박맛');
+INSERT INTO ICE VALUES ('막대' , '죠스바 바나나맛');
+INSERT INTO ICE VALUES ('막대' , '죠스바 초코맛');
+
+INSERT INTO ICE VALUES ('막대' , '수박바 수박맛');
+INSERT INTO ICE VALUES ('막대' , '수박바 딸기맛');
+INSERT INTO ICE VALUES ('막대' , '수박바 오레오맛');
+
+INSERT INTO ICE VALUES ('막대' , '메로나 딸기맛');
+INSERT INTO ICE VALUES ('막대' , '메로나 메론맛');
+INSERT INTO ICE VALUES ('막대' , '메로나 바나나맛');
+INSERT INTO ICE VALUES ('막대' , '메로나 코코넛맛');
+
+INSERT INTO ICE VALUES ('막대' , '보석바 우유맛');
+INSERT INTO ICE VALUES ('막대' , '보석바 바나나맛');
+INSERT INTO ICE VALUES ('막대' , '보석바 치즈크림맛');
+INSERT INTO ICE VALUES ('막대' , '보석바 밤맛');
+INSERT INTO ICE VALUES ('막대' , '보석바 코코넛맛');
+
+
+INSERT INTO ICE VALUES ('컵' , '');
+INSERT INTO ICE VALUES ('컵' , '');
+INSERT INTO ICE VALUES ('컵' , '');
+INSERT INTO ICE VALUES ('컵' , '');
+INSERT INTO ICE VALUES ('컵' , '');
+
+
+SELECT * FROM ICE 
+DROP TABLE ICE 
 
 COMMIT;
+
+
 
 
 
